3eed17f43cfe7447b91391216f331fc1fafae24a
Szymon Samus <siizmonsus@gmail.com>
2025-04-11 22:44:32 +0200

    FIX: Forgot to alter the saveGuildConfig to remove the color & icon properties.

--- services/guildService.js
+++ services/guildService.js
@@ -2,14 +2,12 @@ const db = require('../utils/db');
 
 async function saveGuildConfig(config) {
     const query = `
-        INSERT INTO guilds (id, channel, color, primary_role, admin_role, icon, title, password, created_at, updated_at)
-        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
+        INSERT INTO guilds (id, channel, primary_role, admin_role, title, password, created_at, updated_at)
+        VALUES ($1, $2, $3, $4, $5, $6, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
         ON CONFLICT (id) DO UPDATE
         SET channel = EXCLUDED.channel,
-            color = EXCLUDED.color,
             primary_role = EXCLUDED.primary_role,
             admin_role = EXCLUDED.admin_role,
-            icon = EXCLUDED.icon,
             title = EXCLUDED.title,
             password = EXCLUDED.password,
             updated_at = CURRENT_TIMESTAMP;
@@ -17,10 +15,8 @@ async function saveGuildConfig(config) {
     const values = [
         config.guild,
         config.channel,
-        config.color,
         config.primaryRole,
         config.adminRole,
-        config.icon,
         config.title,
         config.password,
     ];
